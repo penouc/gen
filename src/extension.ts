e33d728147efac3c649ace088f3779bab6ec6927
彭磊 <penouc@gmail.com>
2018-09-12 22:05:18 +0800

    feat: 生成颜色数组

---
+++ src/extension.ts
@@ -0,0 +1,36 @@
+"use strict";
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import { colors } from "./colors";
+
+const Range = vscode.Range;
+const Position = vscode.Position;
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "congen" is now active!');
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+
+  let disposable = vscode.commands.registerTextEditorCommand(
+    "extension.gen.color.green",
+    (editor, edit, args) => {
+      editor.edit(textEditorEdit => {
+        textEditorEdit.insert(
+          editor.selection.active,
+          JSON.stringify(colors[0])
+        );
+      });
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
